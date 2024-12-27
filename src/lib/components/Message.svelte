f7845c6494588a9866db3123a80401bf39a4f5be
jkhasizada <javid.khasizada@gmail.com>
2024-12-28 00:30:10 +0400

    Update message in English

--- src/lib/components/Message.svelte
+++ src/lib/components/Message.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-  
 	import { STATE } from '$lib/stores.js';
 	export let languageValue: string;
 
@@ -18,23 +17,36 @@
 		{#if languageValue == 'en'}
 			<h1>Happy New Year, {nameValue}!!!</h1>
 			<p>
-				I wish you to have people around you who understand you and support you regardless of time
-				and circumstances.
+				I'm glad I found time to work on this project again this year. Not sure if she'll read this,
+				but I'm grateful she reminded me about it. Between work and personal matters, I had
+				completely forgotten.
 			</p>
 			<p>
-				For this year (like last year), I wish you to pursue your own dreams and goals, not those
-				influenced by others. The most important thing is to follow what is right and exactly what
-				suits you.
+				This time of year always feels bittersweet. It's probably the only time I slow down to
+				reflect on the past year and think about ways to make the next one better (not the best
+				habit, but hopefully it's just me).
 			</p>
 			<p>
-				One of the biggest skills I learned in 2022 was how to communicate better with people, and I
-				hope your life is filled with people who have this skill.
+				Looking back, one thing is clear — what truly matters is the people around you. Without
+				them, you're not really you, and your memories wouldn't mean much. Make time for those who
+				care about you. Be there for them — not out of obligation, but because you want to be. And
+				if you feel like you don't have such people, look closer or put your glasses on — they're
+				probably just waiting to be noticed. :)
 			</p>
-			<p>Happy New Year. You are loved!</p>
-			<br />
 			<p>
-				With hearty greetings from Hungary, <span class="signature">Javid "JAXA" Khasizada.</span>
+				For me, personal connections are what matter most, and this year made that even clearer.
 			</p>
+			<p>
+				So, have fun, make memories, and appreciate both what you have and what you don't. Oh, and
+				don't be like me — <b>spend less time on social media!</b>
+			</p>
+			<p>
+				<b>Happy New Year!</b> You are loved — maybe not by me personally, but definitely by someone
+				out there!
+			</p>
+			<p>Warm greetings from Hungary,</p>
+
+			<p><span class="signature">Javid "JAXA" Khasizada.</span></p>
 		{:else}
 			<h1>Təbriklər, {nameValue}!!!</h1>
 
@@ -47,13 +59,14 @@
 				adamların olmasını arzu edirəm.
 			</p>
 			<p>
-				Bu il üçün (keçən ildə olduğu kimi) sənə arzu edirəm ki, ətrafdakıların təsiri olan
-				yox, məhz öz istədiyin arzuların və hədəflərin üçün çalışasan. Ən əsası doğru və məhz sənə
-				uyğun olanlarının arxasınca.
+				Bu il üçün (keçən ildə olduğu kimi) sənə arzu edirəm ki, ətrafdakıların təsiri olan yox,
+				məhz öz istədiyin arzuların və hədəflərin üçün çalışasan. Ən əsası doğru və məhz sənə uyğun
+				olanlarının arxasınca.
 			</p>
 			<p>
 				2022-də öyrəndiyim ən böyük bacarıqlarımdan biri insanlarla daha yaxşı kommunikasiya qurmaq
-				olub. Ümid edirəm ki, qarşıdan gələn ildə həyatın bu bacarığa sahib olan insanlarla dolu olar.
+				olub. Ümid edirəm ki, qarşıdan gələn ildə həyatın bu bacarığa sahib olan insanlarla dolu
+				olar.
 			</p>
 			<p>Yeni ilin mübarək. Sevilirsən!</p>
 			<br />
@@ -61,7 +74,6 @@
 				Macarıstandan ürək dolusu salamlarla, <span class="signature">Cavid "JAXA" Xasizadə.</span>
 			</p>
 		{/if}
-
 	</div>
 </section>
 
@@ -75,5 +87,9 @@
 
 	.signature {
 		font-style: italic;
+	}
+	
+	b {
+		font-weight: 700;
 	}
 </style>
