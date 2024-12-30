1f19744c431710815fc2b79285273dd748636467
Javid Khasizada <56765877+khasizadaj@users.noreply.github.com>
2024-12-30 18:39:00 +0400

    Add pagination to songs section (#15)

--- src/lib/components/SubmittedSongs.svelte
+++ src/lib/components/SubmittedSongs.svelte
@@ -34,52 +34,62 @@
 
 	export let languageValue: string;
 	export let songs: Song[] = [];
+
+	let count = songs.length;
+	$: perPage = 7;
+	$: currentPage = 1;
+	$: shortSongs = songs.slice((currentPage - 1) * perPage, currentPage * perPage);
+
+	const updatePagination = (direction: string) => {
+		if (currentPage === 1 && direction === 'previous') {
+			return;
+		}
+		if (currentPage * perPage >= count && direction === 'next') {
+			return;
+		}
+
+		if (direction === 'next') {
+			currentPage++;
+		} else if (direction === 'previous') {
+			currentPage--;
+		}
+	};
 </script>
 
-<section class="section submitted_songs-section section-submitted_songs" id="submitted_songs">
+<section class="section submitted_songs-section section-submitted_songs mb-5" id="submitted_songs">
 	<div class="wrapper submitted_songs-wrapper">
-		{#if languageValue == 'en'}
-			<h1 class="heading">Songs of the year (by you!)</h1>
-			<p>
-				You can submit song of the year for you. You can see submissions of other people down below.
-			</p>
-			<p class="note">
-				If you have missed it, you can add it <a class="link" href="/share-song">from here</a>.
-			</p>
-			<div>
-				{#each songs as song}
-					<div class="song {song.id}" id="song-{song.id}">
-						<div class="details">
-							<h3>"{song.name}" <span class="note">by</span> {song.artist}</h3>
-						</div>
-						<div class="actions">
-							<a href={song.url} target="_blank"><button class="song-link button">Listen</button></a
-							>
-						</div>
+		<h1 class="heading">Songs of the year (by you!)</h1>
+		<p>
+			You can submit song of the year for you. You can see submissions of other people down below.
+		</p>
+		<p class="note">
+			If you have missed it, you can add it <a class="link" href="/share-song">from here</a>.
+		</p>
+		<div class="mt-4 mb-4">
+			{#each shortSongs as song}
+				<div class="song {song.id}" id="song-{song.id}">
+					<div class="details">
+						<h3>"{song.name}" <span class="note">by</span> {song.artist}</h3>
 					</div>
-				{/each}
-			</div>
-		{:else}
-			<h1 class="heading">İlin seçilmiş mahnıları</h1>
-			<p>Sənin və bu sayta gələnlərin əlavə etdiyi mahnıları aşağıda görə bilərsən.</p>
-			<p class="note">
-				İndi görürsənsə, öz sevimli mahnını <a class="link" href="/share-song">buradan</a>
-				əlavə edə bilərsən.
-			</p>
-			<div>
-				{#each songs as song}
-					<div class="song {song.id}" id="song-{song.id}">
-						<div class="details">
-							<h3>"{song.name}" <span class="note">,</span> {song.artist}</h3>
-						</div>
-						<div class="actions">
-							<a href={song.url} target="_blank"><button class="song-link button">Dinlə</button></a
-							>
-						</div>
+					<div class="actions">
+						<a href={song.url} target="_blank"><button class="song-link button">Listen</button></a>
 					</div>
-				{/each}
-			</div>
-		{/if}
+				</div>
+			{/each}
+		</div>
+		<div class="pagination">
+			<button class="button button-secondary" on:click={() => updatePagination('previous')}
+				>{'<'}</button
+			>
+			{#if languageValue == 'en'}
+				<p>Page {currentPage} of {Math.ceil(count / perPage)}</p>
+			{:else}
+				<p>{currentPage} / {Math.ceil(count / perPage)}</p>
+			{/if}
+			<button class="button button-secondary" on:click={() => updatePagination('next')}
+				>{'>'}</button
+			>
+		</div>
 	</div>
 </section>
 
@@ -101,9 +111,8 @@
 		justify-content: space-between;
 		align-items: center;
 		width: 100%;
-		margin-bottom: 2rem;
 		height: auto;
-		padding-block: 0.5rem;
+		padding-block: 1rem;
 		min-height: 60px;
 	}
 
@@ -115,6 +124,23 @@
 		max-width: 200px;
 		padding: 1rem 2rem;
 		min-width: 80px;
+	}
+
+	.pagination {
+		display: flex;
+		justify-content: left;
+		align-items: center;
+		margin-top: 2rem;
+		gap: 2rem;
+	}
+
+	.pagination > p {
+		margin: 0;
+	}
+	.pagination > .button {
+		padding: 1rem;
+		padding-bottom: 1.4rem;
+		min-width: 5rem;
 	}
 
 	@media screen and (max-width: 400px) {
